8f062a07f8fc833821e6a5182667193797f1a364
1Kaido <zaidshaikh964862@gmail.com>
2026-07-06 00:00:15 +0530

    Update config and finalize requirements for deployment

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ MINDEE_API_KEY = os.getenv("MINDEE_API_KEY")
 MODEL_ID = os.getenv("MODEL_ID")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
-#Default Debugging
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "sqlite:///receiptiq.db")
+#Default Debuggin
+
+DATABASE_URL = os.getenv("DATABASE_URL")
